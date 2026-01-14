ac675c9bf8c6e609d649a0a9f68af6fb5625314c
fchery87 <frantzchery@hotmail.com>
2026-01-13 21:41:00 -0500

    ci: fix eslint config for next 16

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+import nextPlugin from "@next/eslint-plugin-next";
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+
+export default [
+  {
+    ignores: [
+      ".next/**",
+      "node_modules/**",
+      "convex/_generated/**",
+      "dist/**",
+    ],
+  },
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: "off",
+    },
+    plugins: {
+      "@next/next": nextPlugin,
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs["core-web-vitals"].rules,
+      "@next/next/no-page-custom-font": "off",
+    },
+  },
+];
